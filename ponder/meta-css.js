affae698e82d37255b4288d38fd1dfd7904ef33e
andejuli <techtrek@hotmail.com>
2025-09-24 12:07:30 -0600

    finished ponder 2

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -30,3 +30,33 @@ if (true) {
 console.log(course); //works fine, course is global
 //console.log(student); //does not work, can't access a block variable outside the block
                     
+
+let selectElem = document.getElementById('webdevlist');
+
+selectElem.addEventListener('change', function(){
+    document.querySelector('#html').style.color = 'black';
+    document.querySelector('#css').style.color = 'black';
+    document.querySelector('#js').style.color = 'black';
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
+})
+                
+const newPara = document.createElement('p');
+newPara.innerText = "added text here";
+
+const topics = document.querySelector('#topics');
+
+topics.appendChild(newPara);
+
+//topics.innerHTML = '<p>This is it</p>';
+
+const image = document.querySelector('img');
+image.setAttribute('src', 'images/logos.jpg');
+image.setAttribute('alt', 'new logo with black background');
+
+//document.body.className = 'blue';
+
+document.body.style.backgroundColor = 'lightblue';
+
+
